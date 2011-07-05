2b37d7a74e2d24f06c8905676029167b3502f6b8
Nils Toedtmann <git-mail@nils.toedtmann.net>
2011-07-05 11:32:25 +0200

    Started empty orcit.py

---
+++ orcit.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# Copyright (C) 2011  Nils Toedtmann  http://nils.toedtmann.net/
+# 
+# This file is part of orcit.
+# 
+# orcit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# orcit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with orcit. If not, see 
+# <http://www.gnu.org/licenses/gpl-2.0.html>
+
+
