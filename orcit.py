17a3f424b2d6b5fcb575ddb5d7636b8aeb60f445
Nils Toedtmann <git-mail@nils.toedtmann.net>
2011-07-05 13:50:56 +0200

    Added basic IRC PRIVMSG functionality

--- orcit.py
+++ orcit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python -u
 
 # Copyright (C) 2011  Nils Toedtmann  http://nils.toedtmann.net/
 # 
@@ -19,3 +19,58 @@
 # <http://www.gnu.org/licenses/gpl-2.0.html>
 
 
+import sys
+import irclib
+
+
+#server = 'chat.freenode.net'
+server = 'pratchett.freenode.net'
+port   = 6667
+nick   = 'akjwefkjaewhfb'
+target = 'aleumhxlaehfxl'
+
+quit_message = 'bye'
+
+
+class irc_client(irclib.SimpleIRCClient):
+
+    def __init__(self, target = None):
+        irclib.SimpleIRCClient.__init__(self)
+        self.target = target
+
+    def on_welcome(self, connection, event):
+        self.listen()
+
+    def on_disconnect(self, connection, event):
+        sys.exit(0)
+
+    def on_privmsg(self, connection, event):
+        print '### Received private message: %s' % event.arguments()
+
+    def listen(self):
+        print '### I am listening. Press <CTRL>-<D> to leave ###'
+        while True:
+            line = sys.stdin.readline()
+            if not line:
+                break
+            self.connection.privmsg(self.target, line)
+        self.connection.quit(quit_message)
+
+
+def main():
+
+    if irclib.is_channel(target):
+        print '### FATAL ERROR: I only do private messaging and cannot join channels!'
+        sys.exit(1)
+
+    ic = irc_client(target)
+    try:
+        ic.connect(server, port, nick)
+    except irclib.ServerConnectionError, IRCErrorMessage:
+        print IRCErrorMessage
+        sys.exit(1)
+    ic.start()
+
+
+if __name__ == "__main__":
+    main()
